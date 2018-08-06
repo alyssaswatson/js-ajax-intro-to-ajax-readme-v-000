5055920f11b22b97d37a64568ffaa0d7d07c7774
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-08-06 23:49:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,3 +16,10 @@ function showRepositories(event, data) {
   document.getElementById("repositories").innerHTML = repoList
 }
 
+function getCommits(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
+}
